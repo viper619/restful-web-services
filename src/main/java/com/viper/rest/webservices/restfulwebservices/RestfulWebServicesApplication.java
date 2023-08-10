ea17f9f97e24f7354445f1df448924de8c98b813
Vishal Malaviya.CDS <vishal.malaviya.cds@morphisec.com>
2023-08-10 14:54:19 +0530

    Initial commit

---
+++ src/main/java/com/viper/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.viper.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
