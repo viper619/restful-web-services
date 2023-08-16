affc285a8db9e39f3ae8387e1450f19de3b5fe10
Vishal Malaviya <vshlmlvliveit@gmail.com>
2023-08-16 16:12:19 +0530

    Create UserController.java

---
+++ src/main/java/com/viper/rest/webservices/restfulwebservices/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.viper.rest.webservices.restfulwebservices.controller;
+
+import com.viper.rest.webservices.restfulwebservices.entity.User;
+import com.viper.rest.webservices.restfulwebservices.exception.ResourceNotFound;
+import com.viper.rest.webservices.restfulwebservices.service.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @GetMapping("/users")
+    public List<User> fetchUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User fetchUser(@PathVariable int id) throws Exception {
+        try{
+            return userDaoService.findById(id);
+        } catch (ResourceNotFound resourceNotFound) {
+            // This is where we use AOP
+            throw new Exception("Not found");
+        }
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).body(savedUser);
+    }
+}
